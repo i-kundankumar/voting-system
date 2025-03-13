e39a45ea4d93241ae667ec9b14f3b727179a58e4
Kundan Kumar <kundankumar09187@outlook.com>
2025-03-13 21:14:24 +0530

    Fixed port binding for deployment

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
