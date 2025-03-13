371c67b53c7d4f6b62ff6e96751a61d1a8d144fc
Kundan Kumar <kundankumar09187@outlook.com>
2025-03-13 21:19:01 +0530

    Fixed port binding issue

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
